2618b63ff61816358f460fc6644249fb7442bc10
72591_Kedar <kedar.jagtap999@gmail.com>
2025-06-19 16:35:51 +0530

    creating temporary card

--- ECommerce_ReactJS/src/App.jsx
+++ ECommerce_ReactJS/src/App.jsx
@@ -22,8 +22,13 @@ function App() {
         </ul>
       </nav>
 
-      <div className='w-[80%] h-full bg-amber-100'>
-
+      <div className='w-[80%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto  '>
+          <div className='mr-3 mb-3 card p-5 border shadow rounded w-[20%] h-[30vh] flex flex-col justify-center items-center'>
+              <div className='hover:scale-110 w-full h-[80%] bg-contain bg-no-repeat bg-center mb-3' 
+              style={{backgroundImage: "URL(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)"}}>
+              </div>
+              <h1 className='hover:text-blue-500'>Lorem ipsum dolor sit amet.</h1>
+          </div>
       </div>
    </div>
   )
