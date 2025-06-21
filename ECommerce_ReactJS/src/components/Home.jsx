c92bba34230c3d2d2c90f7333c9f526a1b874874
72591_Kedar <kedar.jagtap999@gmail.com>
2025-06-21 10:46:07 +0530

    apply category filters

--- ECommerce_ReactJS/src/components/Home.jsx
+++ ECommerce_ReactJS/src/components/Home.jsx
@@ -1,20 +1,45 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Nav from './Nav'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import {ProductContext} from '../utils/Context'
 import Loading from '../utils/Loading'
+import axios from '../utils/Axios'
 
 const Home = () => {
 
   const [products] = useContext(ProductContext);
   console.log(products);
 
+  const {search} = useLocation();
+
+  const category = decodeURIComponent(search.split("=")[1]);
+
+  const [filteredProducts, setfilteredProducts] = useState(null)
+
+  const getProductsCategory = async () =>{
+    try {
+      const {data} = await axios.get(`products/category/${category}`)
+      setfilteredProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+      if(category && category != "undefined"){
+        getProductsCategory();
+      }
+      else{
+        setfilteredProducts(products);
+      }
+  },[category,products])
+
   return ( products?
     <>
         <Nav/>
         <div className='w-[80%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto  '>
 
-          {products.map((p,i)=>(
+          {filteredProducts && filteredProducts.map((p,i)=>(
             <Link key={p.id} to={`/details/${p.id}`} className='mr-3 mb-3 card p-5 border shadow rounded w-[20%] h-[30vh] flex flex-col justify-center items-center'>
                 <div className='hover:scale-110 w-full h-[80%] bg-contain bg-no-repeat bg-center mb-3' 
                 style={{backgroundImage: `URL(${p.image})`}}>
--- ECommerce_ReactJS/src/components/Nav.jsx
+++ ECommerce_ReactJS/src/components/Nav.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {ProductContext} from '../utils/Context'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
+
+    const [products] = useContext(ProductContext);
+
+    let distinct_category = products.map((p)=> p.category)
+
+    distinct_category = [...new Set(distinct_category)];
+
+    console.log(distinct_category);
+
+    const color = () => {
+        return `rgba(${(Math.random() * 255).toFixed()}, 
+        ${(Math.random() * 255).toFixed()}, 
+        ${(Math.random() * 255).toFixed()}, 0.4)`;
+      };
+      
+
+    console.log(color());
+   
   return (
     
         <nav className='w-[20%] h-full flex flex-col items-center p-5 bg-zinc-200'>
             <a className='py-3 px-3 border border-blue-400 text-blue-500 rounded' href='/create'>Add New Product</a>
             <hr className='w-full mt-2'/>
             <h1 className='text-xl font-bold w-[80%] mb-3'>Category</h1>
-            <ul className='w-[80%]'>
-                <li className='flex items-center mb-3'>
-                    <span className='bg-blue-200 w-[15px] h-[15px] rounded-full mr-2 '></span>Category 1
-                </li>
-                <li className='flex items-center mb-3'>
-                    <span className='bg-red-200 w-[15px] h-[15px] rounded-full mr-2 '></span>Category 2
-                </li>
-                <li className='flex items-center mb-3'>
-                    <span className='bg-purple-200 w-[15px] h-[15px] rounded-full mr-2 '></span>Category 3
-                </li>
-            </ul>
+            <div className='w-[80%]'>
+                {distinct_category.map((c,i)=>(
+
+                <Link 
+                key={i} 
+                to={`/?category=${c}`} 
+                className='flex items-center mb-3'>
+                    <span style={{backgroundColor: color()}} className=' w-[15px] h-[15px] rounded-full mr-2 '></span>{c}
+                </Link>
+
+                ))}
+                
+                
+            </div>
       </nav>
 
   )
